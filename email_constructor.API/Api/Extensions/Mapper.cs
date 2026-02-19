57748aea6641bda5b119c35957273cfd2e1c8b22
Danil Uktus <damayorov93@gmail.com>
2026-02-19 16:34:56 +0500

    feat: EMAIL-1 отрефакторен маппинг

--- email_constructor.API/Api/Extensions/Mapper.cs
+++ email_constructor.API/Api/Extensions/Mapper.cs
@@ -10,39 +10,32 @@ public interface IMapper
 }
 public class Mapper : IMapper
 {
-    public ContentData MapToContentData(GetEmailContentBlockRequest request)
-    {
-        return new ContentData
+    public ContentData MapToContentData(GetEmailContentBlockRequest request) =>
+        new()
         {
             LanguageId = request.LanguageId,
             StoreId = request.StoreId,
             Blocks = request.Blocks.Select(MapToBlockData).ToList(),
         };
-    }
 
-    private static BlockData MapToBlockData(Block block)
-    {
-        return new BlockData
+    private static BlockData MapToBlockData(Block block) => 
+        new()
         {
             Type = block.Type,
             Payload = block.Payload.ToDictionary(),
         };
-    }
     
-    public GetEmailContentBlockResponse MapToResponse(List<BlockData> blocks)
-    {
-        return new GetEmailContentBlockResponse()
+    public GetEmailContentBlockResponse MapToResponse(List<BlockData> blocks) =>
+        new()
         {
             Blocks = { blocks.Select(MapToBlock) }
         };
-    }
     
-    private static Block MapToBlock(BlockData blockData)
-    {
-        return new Block
+    
+    private static Block MapToBlock(BlockData blockData) => 
+        new()
         {
             Type = blockData.Type, 
             Payload = { blockData.Payload }
         };
-    }
 }
